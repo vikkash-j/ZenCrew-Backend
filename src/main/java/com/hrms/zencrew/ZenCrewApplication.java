9413e77da5bf261fad6c7fec41f9d6e1a2a172eb
unknown <vikkashjayam@gmail.com>
2026-07-24 22:44:53 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/hrms/zencrew/ZenCrewApplication.java
@@ -0,0 +1,13 @@
+package com.hrms.zencrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenCrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenCrewApplication.class, args);
+	}
+
+}
